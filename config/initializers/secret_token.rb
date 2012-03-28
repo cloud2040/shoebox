3318e2a5ef011196321f6ae1d43ff013e8b2d628
Cloud2040 <cloud2040@gmail.com>
2012-03-28 07:00:12 -0400

    First commit of shoebox project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shoebox::Application.config.secret_token = '5a7ded2614537a95e53867fb51faa27d40c765d6e0b75b33ce2394d0c71251d0a47a2d33a25967f39d3f2c96e9cd6c30c383aaac11d7788cc29fd6cae6f3f400'
